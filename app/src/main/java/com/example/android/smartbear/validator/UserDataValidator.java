4224029ae9301cb3d3ffd92c6f43c58ee4381e0d
dimas1318 <parshin@phystech.edu>
2017-10-20 19:50:02 +0300

    add Validator, exceptions + refactoring

---
+++ app/src/main/java/com/example/android/smartbear/validator/UserDataValidator.java
@@ -0,0 +1,57 @@
+package com.example.android.smartbear.validator;
+
+import com.example.android.smartbear.validator.exception.NotValidDataException;
+import com.example.android.smartbear.validator.exception.TooLondTextException;
+import com.example.android.smartbear.validator.exception.TooShortTextException;
+
+/**
+ * Created by parsh on 20.10.2017.
+ */
+
+public class UserDataValidator {
+    public static void validateName(String name) throws TooShortTextException {
+        if (name.isEmpty() || name.length() < 3) {
+            throw new TooShortTextException();
+        }
+    }
+
+    public static void validateAddress(String address) throws TooShortTextException {
+        if (address.isEmpty()) {
+            throw new TooShortTextException();
+        }
+    }
+
+    public static void validateEmail(String email) throws TooShortTextException {
+        if (email.isEmpty()) {
+            throw new TooShortTextException();
+        }
+    }
+
+    public static void validateMobile(String mobile) throws TooShortTextException, NotValidDataException {
+        if (mobile.isEmpty() ) {
+            throw new TooShortTextException();
+        } else if (mobile.length() != 10) {
+            throw new NotValidDataException();
+        }
+    }
+
+    public static void validatePassword(String password) throws TooShortTextException, TooLondTextException {
+        if (password.isEmpty() || password.length() < 4) {
+            throw new TooShortTextException();
+        } else if (password.length() > 10) {
+            throw new TooLondTextException();
+        }
+    }
+
+    public static void validateReEnterPassword(String reEnterPassword, String password) throws NotValidDataException, TooShortTextException, TooLondTextException {
+        if (!password.equals(reEnterPassword)) {
+            throw new NotValidDataException();
+        } else {
+            if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4) {
+                throw new TooShortTextException();
+            } else if (reEnterPassword.length() > 10) {
+                throw new TooLondTextException();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/smartbear/validator/exception/NotValidDataException.java
@@ -0,0 +1,8 @@
+package com.example.android.smartbear.validator.exception;
+
+/**
+ * Created by parsh on 20.10.2017.
+ */
+
+public class NotValidDataException extends Throwable {
+}
